8718c89f50c12cb735117a25906f635279affbd2
Alexander Shoshin <Alexander_Shoshin@epam.com>
2016-07-25 12:22:43 +0300

    add tests

---
+++ src/test/java/shoshin/alex/utils/MapSupplyTest.java
@@ -0,0 +1,46 @@
+package shoshin.alex.utils;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ *
+ * @author Alexander_Shoshin
+ */
+public class MapSupplyTest {
+    Map<String, Integer> targetMap;
+    
+    @Before
+    public void initTargetMap() {
+        targetMap = new HashMap<>();
+        for (int i = 0; i < 10; i++) {
+            targetMap.put("key" + i, i);
+        }
+    }
+    
+    @Test
+    public void should_find_max_values_keys() {
+        List<String> maxKeys = MapSupply.getMaxValueKeys(targetMap, 3);
+        Collections.sort(maxKeys);
+        Assert.assertArrayEquals(new String[]{"key7", "key8", "key9"}, maxKeys.toArray(new String[0]));
+    }
+    
+    @Test
+    public void should_substring_map() {
+        Map<String, Integer> expectedMap = new HashMap<>();
+        expectedMap.put("key1", 1);
+        expectedMap.put("key5", 5);
+        expectedMap.put("key4", 4);
+        List<String> keysToSubstring = new LinkedList<>();
+        Collections.addAll(keysToSubstring, expectedMap.keySet().toArray(new String[0]));
+        Map<String, Integer> subMap = MapSupply.substring(targetMap, keysToSubstring);
+        Assert.assertEquals(expectedMap, subMap);
+    }
+}
